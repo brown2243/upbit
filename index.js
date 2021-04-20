3fded558dde4d588dae8139703ef8c960bbfaed5
brown2243 <brown2243@naver.com>
2021-04-20 23:36:58 +0900

    evolve

--- index.js
+++ index.js
@@ -9,71 +9,76 @@ const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY;
 const cheapCoins = [
   "KRW-ONG",
   "KRW-PLA",
-  // "KRW-SSX",
-  // "KRW-CBK",
-  // "KRW-TON",
-  // "KRW-HUNT",
+  "KRW-SSX",
+  "KRW-CBK",
+  "KRW-TON",
+  "KRW-HUNT",
   // "KRW-AQT",
-  // "KRW-PXL",
-  // "KRW-SBD",
-  // "KRW-QTCON",
-  // "KRW-DMT",
-  // "KRW-MOC",
-  // "KRW-OBSR",
-  // "KRW-UPP",
+  "KRW-PXL",
+  "KRW-SBD",
+  "KRW-QTCON",
+  "KRW-DMT",
+  "KRW-MOC",
+  "KRW-OBSR",
+  "KRW-UPP",
   // "KRW-STPT",
-  // "KRW-EDR",
-  // "KRW-AERGO",
-  // "KRW-HUM",
+  "KRW-EDR",
+  "KRW-AERGO",
+  "KRW-HUM",
   // "KRW-EMC2",
-  // "KRW-IGNIS",
-  // "KRW-RFR",
-  // "KRW-SOLVE",
-  // "KRW-LBC",
+  "KRW-IGNIS",
+  "KRW-RFR",
+  "KRW-SOLVE",
+  "KRW-LBC",
   // "KRW-GRS",
-  // "KRW-MBL",
-  // "KRW-STRK",
-  // "KRW-LAMB",
-  // "KRW-TT",
-  // "KRW-MLK",
+  "KRW-MBL",
+  "KRW-STRK",
+  "KRW-LAMB",
+  "KRW-TT",
+  "KRW-MLK",
   // "KRW-ADX",
-  // "KRW-GAS",
-  // "KRW-CRE",
+  "KRW-GAS",
+  "KRW-CRE",
   // "KRW-LOOM",
-  // "KRW-DKA",
-  // "KRW-MARO",
-  // "KRW-MFT",
-  // "KRW-BORA",
+  "KRW-DKA",
+  "KRW-MARO",
+  "KRW-MFT",
+  "KRW-BORA",
   // "KRW-DAWN",
-  // "KRW-POWR",
-  // "KRW-HIVE",
-  // "KRW-TSHP",
-  // "KRW-META",
-  // "KRW-QKC",
-  // "KRW-POLY",
+  "KRW-POWR",
+  "KRW-HIVE",
+  "KRW-TSHP",
+  "KRW-META",
+  "KRW-QKC",
+  "KRW-POLY",
   // "KRW-ELF",
-  // "KRW-MTL",
-  // "KRW-IQ",
-  // "KRW-ARK",
+  "KRW-MTL",
+  "KRW-IQ",
+  "KRW-ARK",
   // "KRW-AXS",
-  // "KRW-SRM",
-  // "KRW-MED",
-  // "KRW-STRAX",
-  // "KRW-WAXP",
-  // "KRW-ORBS",
-  // "KRW-JST",
-  // "KRW-MVL",
+  "KRW-SRM",
+  "KRW-MED",
+  "KRW-STRAX",
+  "KRW-WAXP",
+  "KRW-ORBS",
+  "KRW-JST",
+  "KRW-MVL",
+  "KRW-KAVA",
+  "KRW-SXP",
+  "KRW-KMD",
+  // "KRW-CVC",
+  // "KRW-ARDR",
+  "KRW-STEEM",
+  "KRW-SAND",
+  "KRW-GLM",
+  "KRW-STMX",
+  "KRW-REP",
 ];
-// .forEach((v, i) => console.log(i, v)); // 55개
-
-// async function sleep(ms) {
-//   const wakeUpTime = Date.now() + ms;
-//   while (Date.now() < wakeUpTime) {}
-// }
+// .forEach((v, i) => console.log(i, v));
 
 async function check6daysAVG(upbit, coin) {
   const data = await upbit.market_day(coin, null, 6);
-  console.log(data.remain_sec, data.remain_min);
+  // console.log(data.remain_sec, data.remain_min);
   if (!data.data) {
     console.log("check5daysAVG error", coin, data.data);
     return { coin: coin, error: true };
@@ -86,57 +91,75 @@ async function check6daysAVG(upbit, coin) {
       return acc + cur["trade_price"];
     }, 0) / 5;
 
-  const target = Number(
-    (
-      (data.data[1].high_price - data.data[1].low_price) * 0.49 +
-      data.data[0].opening_price
-    ).toFixed(4)
+  const gap = Number(
+    (data.data[1].high_price - data.data[1].low_price) * 0.495
   );
-  return { coin: coin, AVG: AVG, target: target };
+  // const target = Number((gap + data.data[0].opening_price).toFixed(4));
+  // console.log(
+  //   "COIN",
+  //   coin,
+  //   "GAP",
+  //   gap.toFixed(3),
+  //   "AVG",
+  //   AVG,
+  //   "T_Price",
+  //   target
+  // );
+  const buy = data.data[0].opening_price + gap * 0.15;
+  const sell = data.data[0].opening_price + gap;
+
+  return {
+    coin: coin,
+    avg: AVG,
+    opening: data.data[0].opening_price,
+    buy: buy,
+    sell: sell,
+  };
+  // return { coin: coin, AVG: AVG, target: target };
 }
 
-// 한번에 다 가져오면, 갯수제한에 걸려서 안되는가 싶었다.
-// 10개 초과하면 null
-// 10개 받고 지연을 하든, 한번받고 지연한번을 하든 10개이상은 안줌.
-// 최악의 경우에는 매일 내가 손으로 해야할 수도...
-// settimeout으로 해결
 async function init() {
   console.log("시작합니다.");
   const upbit = new Upbit(secret_key, access_key);
 
   const account = await upbit.accounts();
   const KRW = Number(account.data[0].balance);
-  const CNT = 10;
+  const CNT = 14;
   const splitedKRW = Math.floor(KRW / CNT) - 100;
+  console.log(`${CNT} 분할, ${splitedKRW}원`);
   const wallet = { KRW: splitedKRW, cnt: CNT };
 
   const DP_5daysAvg = {};
   cheapCoins.forEach(async (coin, i) => {
     setTimeout(async () => {
       const value = await check6daysAVG(upbit, coin);
+      // if (value["AVG"] <= value["target"]) {
+      // 5일 평균보다 매수가가 높아야 됨, 즉 우상향차트
+      // DP_5daysAvg[value["coin"]] = {
+      //   AVG: value["AVG"],
+      //   target: value["target"],
+      // };
       DP_5daysAvg[value["coin"]] = {
-        AVG: value["AVG"],
-        target: value["target"],
+        opening: value["opening"],
+        buy: value["buy"],
+        sell: value["sell"],
       };
+      // }
     }, i * 180);
   });
   setTimeout(() => {
     serverStart(DP_5daysAvg, wallet);
   }, cheapCoins.length * 200);
 }
-// init();
 
 async function buyOrder(ticker, wallet) {
-  console.log("buyOrder", ticker, wallet);
   const upbit = new Upbit(secret_key, access_key);
-
   const order = await upbit.order_bid(ticker, wallet["KRW"]);
-  console.log(order);
 
-  // 카톡 알림 기능 구현
-  //
+  console.log("buyOrder", ticker, wallet, order);
 
   wallet["cnt"] = wallet["cnt"] - 1;
+  console.log(`매수 횟수가 ${wallet["cnt"]}회 남았습니다.`);
 }
 
 async function serverStart(targetList, wallet) {
@@ -147,7 +170,7 @@ async function serverStart(targetList, wallet) {
   //   process.exit();
   // }
 
-  console.log("마켓수:" + code_list.length);
+  console.log("매수 대기 코인 갯수: " + code_list.length);
   // 체결 서버 접속
   tradeServerConnect(code_list.join(","), targetList, wallet);
 }
@@ -171,45 +194,56 @@ function tradeServerConnect(codes, targetList, wallet) {
     //   tradeServerConnect(codes, targetList, wallet);
     // }, 1000);
   });
-  ws.on("message", (data) => {
+  ws.on("message", async (data) => {
     try {
       const str = data.toString("utf-8");
       const json = JSON.parse(str);
-      if (targetList[json.cd]) {
-        // console.log(json.cd, json.tp, targetList[json.cd]);
+      if (targetList[json.cd] && targetList[json.cd].buy <= json.tp) {
+        console.log(json.cd, json.tp, targetList[json.cd]);
+        console.log(json.cd, "코인 매수 진행");
+        // 매수 함수 실행
+        buyOrder(json.cd, wallet);
+        wallet[json.cd] = { ...targetList[json.cd], isSelled: false };
+        console.log(wallet[json.cd]);
         if (wallet.cnt <= 0) {
           console.log("매수가 완료되었습니다!!");
           ws.close();
         }
-
-        if (
-          json.tp >= targetList[json.cd].AVG &&
-          json.tp >= targetList[json.cd].target
+        delete targetList[json.cd];
+        // serverStart(targetList);
+      } else if (wallet[json.cd]) {
+        if (wallet[json.cd].isSelled && wallet[json.cd].buy >= json.tp * 1.1) {
+          console.log(`${json.cd}코인, 현재가${json.tp}`);
+          console.log(`매수가 보다 낮아져 다시 편입`);
+          targetList[json.cd] = {
+            ...wallet[json.cd],
+          };
+          delete wallet[json.cd];
+        } else if (
+          !wallet[json.cd].isSelled &&
+          wallet[json.cd].sell <= json.tp
         ) {
-          console.log(json.cd, json.tp, targetList[json.cd]);
-          console.log(json.cd, "코인 매수 진행하고 시세체크 멈춤");
-          // 매수 함수 실행
-          // 코인 이름, 매수가
-          // 잔고가 남았다면
-          buyOrder(json.cd, wallet, ws);
-          if (wallet.cnt <= 0) {
-            console.log("매수가 완료되었습니다!!");
-            ws.close();
-          }
-          delete targetList[json.cd];
-          serverStart(targetList);
-        }
+          const upbit = new Upbit(secret_key, access_key);
+          console.log(
+            `${json.cd}코인 현재가${json.tp}, 매도가${
+              wallet[json.cd]
+            } 매도 진행`
+          );
+          wallet[json.cd].isSelled = true;
+          const account = await upbit.accounts();
 
-        // 매수조건에 맞는 종목이 없어서(하락장)
-        // 코드 작동여부 확인 위해 하방으로 설정해봤음.
-        // if (
-        //   json.tp <= targetList[json.cd].AVG &&
-        //   json.tp <= targetList[json.cd].target
-        // ) {
-        //   console.log("낮다.");
-        //   delete targetList[json.cd];
-        //   start(targetList);
-        // }
+          account.data.forEach(async (coin) => {
+            if (json.cd === coin.currency) {
+              const sellOrder = await upbit.order_ask(
+                `KRW-${coin.currency}`,
+                coin.balance
+              );
+              console.log(sellOrder);
+            }
+          });
+
+          wallet["cnt"] = wallet["cnt"] + 1;
+        }
       }
     } catch (e) {
       console.log(e);
@@ -226,17 +260,22 @@ setInterval(async () => {
   const hours = date.substr(0, 2);
   const mins = date.substr(3, 2);
   console.log(`${hours}:${mins}`);
-  if (hours === "08" && mins === "58") {
-    console.log("오전 08:58, 전량 매도 작동.");
+  if (hours === "09" && mins === "18") {
+    console.log("오전 08:57, 전량 매도 작동.");
     allSell();
   }
 
-  if (hours === "09" && mins === "01") {
+  if (hours === "09" && mins === "19") {
     console.log("오전 09:01, 매수 프로그램 다시 시작.");
+
+    fs.appendFile("./trading.txt", date + "\n", "utf8", (err) => {
+      console.log(err);
+    });
     init();
   }
-}, 1000);
+}, 60000);
 
+init(); // 그냥 시작하려면
 async function allSell() {
   const upbit = new Upbit(secret_key, access_key);
   const account = await upbit.accounts();
@@ -247,14 +286,33 @@ async function allSell() {
     }
 
     setTimeout(async () => {
-      console.log(coin.currency, coin.balance);
-      const sellOrder = await upbit.order_ask(coin.currency, coin.balance);
+      console.log(coin);
+      const sellOrder = await upbit.order_ask(
+        `KRW-${coin.currency}`,
+        coin.balance
+      );
+      fs.appendFile(
+        "./trading.txt",
+        `매도 ${coin.currency} , 금액 ${coin.balance}, ${sellOrder}\n`,
+        "utf8",
+        (err) => {
+          console.log(err);
+        }
+      );
       console.log(sellOrder);
-    }, i * 200);
+    }, i * 400);
   });
 
   setTimeout(async () => {
     const account = await upbit.accounts();
-    console.log("전량 매도후, 현금", account.data[0]);
+    console.log("전량 매도후, 현금 :", account.data[0]);
+    fs.appendFile(
+      "./trading.txt",
+      "전량 매도후, 현금 :" + account.data[0] + "\n",
+      "utf8",
+      (err) => {
+        console.log(err);
+      }
+    );
   }, account.length * 300);
 }
--- kakao_lib.js
+++ kakao_lib.js
@@ -1,5 +1,51 @@
-const moment = require("moment");
-require("moment-timezone");
-moment.tz.setDefault("Asia/Seoul");
-const date = moment().format("YYYY-MM-DD HH:mm:ss");
-console.log(date);
+// require("dotenv").config();
+
+// const KAKAO_NATIVE_KEY = process.env.KAKAO_NATIVE_KEY;
+// const KAKAO_RESTAPI_KEY = process.env.KAKAO_RESTAPI_KEY;
+// const KAKAO_JS_KEY = process.env.KAKAO_JS_KEY;
+// const KAKAO_ADMIN_KEY = process.env.KAKAO_ADMIN_KEY;
+// const redirect_uri = "https://example.com/oauth";
+
+// const request = require("request");
+// const querystring = require("querystring");
+
+// let template_objectObj = {
+//   object_type: "text",
+//   text: " Hello Kakao!(텍스트 영역입니다. 최대 200자 표시 가능합니다.)",
+//   link: {
+//     web_url: "https://developers.kakao.com",
+//     mobile_web_url: "https://developers.kakao.com",
+//   },
+// };
+
+// // Javascript -> JSON 타입으로 변경
+// let template_objectStr = JSON.stringify(template_objectObj);
+// let options = {
+//   url: "https://kapi.kakao.com/v2/api/talk/memo/default/send",
+//   method: "POST",
+
+//   headers: {
+//     Authorization: "Bearer xxxxxxxxxxxxxxxxxxxxxx",
+//     "Content-Type": "application/x-www-form-urlencoded",
+//   },
+//   form: {
+//     template_object: template_objectStr,
+//   },
+// };
+
+// function callback(error, response, body) {
+//   console.log(response.statusCode);
+//   if (!error && response.statusCode == 200) {
+//     console.log(body);
+//   }
+// }
+// request(options, callback);
+
+// const fs = require("fs");
+// const data = "SADASdASdasdda";
+// fs.writeFile("./trading.txt", data + "\n", "utf8", (err) => {
+//   console.log(err);
+// });
+// fs.appendFile("./trading.txt", data, "utf8", (err) => {
+//   console.log(err);
+// });
